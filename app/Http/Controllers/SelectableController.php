f67a3ac3e7b83b9b0db1f5d20af6a47c75ee115c
Thijs <thijsbrandes@hotmail.com>
2023-12-15 13:05:19 +0100

    extra table toegevoegd

---
+++ app/Http/Controllers/SelectableController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\selectable;
+use App\Http\Requests\StoreselectableRequest;
+use App\Http\Requests\UpdateselectableRequest;
+
+class SelectableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreselectableRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(selectable $selectable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(selectable $selectable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateselectableRequest $request, selectable $selectable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(selectable $selectable)
+    {
+        //
+    }
+}
