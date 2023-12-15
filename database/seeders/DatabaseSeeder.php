0c24738603ceba337a3f14270905c457b9145703
Thijs <thijsbrandes@hotmail.com>
2023-12-15 01:01:09 +0100

    hele hoop shit toegevoegd aan de database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,18 +20,57 @@ class DatabaseSeeder extends Seeder
          ]);
 
         \App\Models\Race::factory()->create([
-            'name' => 'Monza', 'location' => 'Monza', 'length' => 5.793,
-            'name' => 'Silverstone', 'location' => 'Silverstone', 'length' => 5.891,
-            'name' => 'Spa-Francorchamps', 'location' => 'Stavelot', 'length' => 7.004,
-            'name' => 'Monaco', 'location' => 'Monte Carlo', 'length' => 3.337,
-            'name' => 'Suzuka', 'location' => 'Suzuka', 'length' => 5.807,
-            'name' => 'Circuit of the Americas', 'location' => 'Austin', 'length' => 5.513,
-            'name' => 'Baku City Circuit', 'location' => 'Baku', 'length' => 6.003,
-            'name' => 'Melbourne Grand Prix Circuit', 'location' => 'Melbourne', 'length' => 5.303,
-            'name' => 'Shanghai International Circuit', 'location' => 'Shanghai', 'length' => 5.451,
-            'name' => 'Hockenheimring', 'location' => 'Hockenheim', 'length' => 4.574,
-            // Add more tracks here'
+            'name' => 'Monza',
+            'location' => 'Monza',
+            'length' => 5.793,
             ]);
+        \App\Models\Race::factory()->create([
+            'name' => 'Silverstone',
+            'location' => 'Silverstone',
+            'length' => 5.891,
+            ]);
+        \App\Models\Race::factory()->create([
+            'name' => 'Spa-Francorchamps',
+            'location' => 'Stavelot',
+            'length' => 7.004,
+            ]);
+        \App\Models\Race::factory()->create([
+            'name' => 'Monaco',
+            'location' => 'Monte Carlo',
+            'length' => 3.337,
+            ]);
+        \App\Models\Race::factory()->create([
+            'name' => 'Suzuka',
+            'location' => 'Suzuka',
+            'length' => 5.807,
+            ]);
+        \App\Models\Race::factory()->create([
+            'name' => 'Circuit of the Americas',
+            'location' => 'Austin',
+            'length' => 5.513,
+            ]);
+        \App\Models\Race::factory()->create([
+            'name' => 'Baku City Circuit',
+            'location' => 'Baku',
+            'length' => 6.003,
+            ]);
+        \App\Models\Race::factory()->create([
+            'name' => 'Melbourne Grand Prix Circuit',
+            'location' => 'Melbourne',
+            'length' => 5.303,
+            ]);
+        \App\Models\Race::factory()->create([
+            'name' => 'Shanghai International Circuit',
+            'location' => 'Shanghai',
+            'length' => 5.451,
+            ]);
+        \App\Models\Race::factory()->create([
+            'name' => 'Hockenheimring',
+            'location' => 'Hockenheim',
+            'length' => 4.574,
+            ]);
+            // Add more tracks here'
+
 
 
     }
