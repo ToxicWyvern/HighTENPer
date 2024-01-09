98865132c14613ad1fc76fbd4593f6acd9d4956a
NiloyanSellathurai02 <n.sella2001@gmail.com>
2024-01-09 11:28:47 +0100

    did some changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,22 +45,79 @@
                 <div class="leaderboard">
                     <div class="leaderboard-property">
                         <div class="rank">Rank</div>
-                        <div class="time">Time</div>
                         <div class="driver">Driver</div>
+                        <div class="time">Time</div>
                         <div class="team">Team</div>
                         <div class="tires">Tires</div>
                     </div>
                     <div class="leaderboard-value">
                         <div class="rank-value"># 1</div>
-                        <div class="time-value">1:20:15</div>
                         <div class="driver-value">Niloyan</div>
+                        <div class="time-value">1:20:15</div>
                         <div class="team-value">Red Bull</div>
                         <div class="tires-value">Hard</div>
                     </div>
                     <div class="leaderboard-value">
                         <div class="rank-value"># 2</div>
-                        <div class="time-value">1:31:25</div>
                         <div class="driver-value">Ebram</div>
+                        <div class="time-value">1:31:25</div>
+                        <div class="team-value">Mercedes</div>
+                        <div class="tires-value">Hard</div>
+                    </div>
+
+                      <div class="leaderboard-value">
+                        <div class="rank-value"># 3</div>
+                        <div class="driver-value">Thijs</div>
+                        <div class="time-value">1:31:25</div>
+                        <div class="team-value">Mercedes</div>
+                        <div class="tires-value">Hard</div>
+                    </div>
+                      <div class="leaderboard-value">
+                        <div class="rank-value"># 4</div>
+                        <div class="driver-value">Pedro</div>
+                        <div class="time-value">1:40:34</div>
+                        <div class="team-value">Mercedes</div>
+                        <div class="tires-value">Hard</div>
+                    </div>
+                      <div class="leaderboard-value">
+                        <div class="rank-value"># 5</div>
+                        <div class="driver-value">Renas</div>
+                        <div class="time-value">1:41:25</div>
+                        <div class="team-value">Mercedes</div>
+                        <div class="tires-value">Hard</div>
+                    </div>
+                      <div class="leaderboard-value">
+                        <div class="rank-value"># 6</div>
+                        <div class="driver-value">Ebram</div>
+                        <div class="time-value">1:44:45</div>
+                        <div class="team-value">Mercedes</div>
+                        <div class="tires-value">Hard</div>
+                    </div>
+                      <div class="leaderboard-value">
+                        <div class="rank-value"># 7</div>
+                        <div class="driver-value">Ebram</div>
+                        <div class="time-value">1:31:25</div>
+                        <div class="team-value">Mercedes</div>
+                        <div class="tires-value">Hard</div>
+                    </div>
+                      <div class="leaderboard-value">
+                        <div class="rank-value"># 8</div>
+                        <div class="driver-value">Ebram</div>
+                        <div class="time-value">1:31:25</div>
+                        <div class="team-value">Mercedes</div>
+                        <div class="tires-value">Hard</div>
+                    </div>
+                      <div class="leaderboard-value">
+                        <div class="rank-value"># 9</div>
+                        <div class="driver-value">Ebram</div>
+                        <div class="time-value">1:31:25</div>
+                        <div class="team-value">Mercedes</div>
+                        <div class="tires-value">Hard</div>
+                    </div>
+                      <div class="leaderboard-value">
+                        <div class="rank-value"># 10</div>
+                        <div class="driver-value">Ebram</div>
+                        <div class="time-value">1:31:25</div>
                         <div class="team-value">Mercedes</div>
                         <div class="tires-value">Hard</div>
                     </div>
