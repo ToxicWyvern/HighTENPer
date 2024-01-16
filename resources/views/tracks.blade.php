93c5acb6b2aced208a7d2642a5a5a36fc36770ec
NiloyanSellathurai02 <n.sella2001@gmail.com>
2024-01-16 18:26:40 +0100

    made the tracks page

--- resources/views/tracks.blade.php
+++ resources/views/tracks.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-
-<h1>Tracks</h1>
-
+<div class="tracks-container">
+    <h1>Tracks</h1>
     @foreach($tracks as $track)
-        <p>{{$track->name}} | {{$track->date}}</p>
+    <div class="tracks">
+      <div class="tracks-name">{{$track->name}} </div>
+      <div class="tracks-date">{{$track->date}}</div>
+    </div>
     @endforeach
+  </div>
 
 
 @endsection
