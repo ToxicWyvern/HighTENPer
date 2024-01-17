f3bc8178747adaa9d562083e6b340330d3671536
NiloyanSellathurai02 <n.sella2001@gmail.com>
2024-01-17 19:27:32 +0100

    Made some styling in the tracks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,7 +59,10 @@
                             @endif
                         @else
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {{ Auth::user()->name }} {{ Auth::user()->trophies }} <img src="images/trophy.png">
+                                {{ Auth::user()->name }} {{ Auth::user()->trophies }} <img src="images/trophy.png" class="trophy-image">
+                            </a>
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="/dashboard" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dashboard
                             </a>
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
