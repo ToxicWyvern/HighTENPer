0188453b3fcc74da161db77e453952cc525b7a7e
Thijs <thijsbrandes@hotmail.com>
2024-01-17 13:08:41 +0100

    chart deels gemaakt, nilo maakt hem af

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
                             @endif
                         @else
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {{ Auth::user()->name }}
+                                {{ Auth::user()->name }} {{ Auth::user()->trophies }} <img src="images/trophy.png">
                             </a>
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
