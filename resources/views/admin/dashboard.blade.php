6a0f98391188b6d399609fa443c7c94408d39128
NiloyanSellathurai02 <n.sella2001@gmail.com>
2024-01-18 21:40:33 +0100

    Made admin dashboard gemaakt

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="dashboard-admin-container">
     @if(auth()->check() && auth()->user()->admin)
         {{-- Admin Dashboard content --}}
-        <h1>Welkom bij het Admin Dashboard, {{ Auth::user()->name }}!</h1>
-
+        <h1 class="dashboard-admin-heading">ADMIN DASHBOARD</h1>
         {{-- Button to Manage Users --}}
-        <a href="{{ route('admin.manageUsers') }}" class="btn btn-primary">Beheer Gebruikers</a>
-
-        <a href="{{ route('admin.manageLeaderboards') }}" class="btn btn-primary">Beheer Leaderboards</a>
+    <div class="dashboard-admin">
+        <div class="dashboard-admin-username">
+            <h2>{{ auth::user()->name }}</h2>
+        </div>
+        <div class="dashboard-admin-cta">
+            <a href="{{ route('admin.manageUsers') }}" class="btn btn-primary">Beheer Gebruikers</a>
+            <a href="{{ route('admin.manageLeaderboards') }}" class="btn btn-primary">Beheer Leaderboards</a>
+        </div>
+    </div>   
 
     @else
         {{-- Unauthorized message --}}
         <p>Je bent niet geautoriseerd om je op deze pagina te bevinden. </p>
     @endif
+</section>
+    
 @endsection
